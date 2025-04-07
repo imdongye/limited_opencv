7b344df241593f12501d2fd5bb86a40727cbace8
임동예 <2025998@vieworks.com>
2025-04-07 10:25:58 +0900

    ready for linux

--- main/main.cpp
+++ main/main.cpp
@@ -5,5 +5,8 @@ int main() {
     cv::Mat img(512, 512, CV_8UC3, cv::Scalar(0, 0, 0));
     std::cout << "Hello, World!" << std::endl;
     cv::VideoCapture cap(0);
+    // cap.get(cv::CAP_PROP_FRAME_WIDTH)
+    std::cout<<cap.get(cv::CAP_PROP_FRAME_WIDTH)<<" "<<cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+
     return 0;
 }
